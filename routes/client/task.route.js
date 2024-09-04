97224b9b106a61423146db68106e41ae48d73118
Do Tien Dat <dotiendat711@gmail.com>
2024-09-04 21:54:41 +0700

    Chỉnh sửa công việc

--- routes/client/task.route.js
+++ routes/client/task.route.js
@@ -7,8 +7,10 @@ router.get("/", controller.index);
 
 router.get("/detail/:id", controller.detail);
 
-router.patch("/change-status", controller.changeStatus)
+router.patch("/change-status", controller.changeStatus);
 
-router.post("/create", controller.create)
+router.post("/create", controller.create);
+
+router.patch("/edit/:id", controller.edit);
 
 module.exports = router;
