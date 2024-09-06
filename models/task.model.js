9e4d661c717cfe5dc0f9df7a62ccec31d3259af8
Do Tien Dat <dotiendat711@gmail.com>
2024-09-06 22:42:17 +0700

    Tạo task con (Lưu thêm id task cha)

--- models/task.model.js
+++ models/task.model.js
@@ -8,6 +8,7 @@ const taskSchema = new mongoose.Schema({
     timeFinish: Date,
     createdBy: String,
     listUser: Array,
+    parentId: String,
     deleted: {
         type: Boolean,
         default: false
